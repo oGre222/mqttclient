5d8c314351754a8a6615c9c7e1959b8cbe2be7ea
马骉 <mabiao@100tal.com>
2018-07-26 15:52:58 +0800

    update MQTT-3.8.1-1

--- src/swoole/message/Subscribe.php
+++ src/swoole/message/Subscribe.php
@@ -14,8 +14,15 @@ use mqttclient\src\swoole\Util;
 
 class Subscribe extends BaseMessage
 {
-
     protected $type = MessageType::SUBSCRIBE;
+
+    /*
+     * MQTT-3.8.1-1
+     * Bits 3,2,1 and 0 of the fixed header of the SUBSCRIBE Control Packet are reserved
+     * and MUST be set to 0,0,1 and 0 respectively.
+     * The Server MUST treat any other value as malformed and close the Network Connection.
+     */
+    protected $reserved_flags = 0x02;
 
     protected $need_message_id = true;
 
